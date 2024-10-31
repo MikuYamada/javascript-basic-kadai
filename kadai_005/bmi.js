99ad30e2f18c1b574958d461b9a0bcc91046f80d
MikuYamada <flydothikin.39@gmail.com>
2024-10-31 22:10:23 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 変数の宣言・値の代入
+let weight = 68; 
+let height = 1.7; 
+
+// 変数の宣言・値の代入
+let bmi = weight / (height * height);
+
+// コンソールへの出力
+console.log("体重: " + weight + "kg");
+console.log("身長: " + height + "m");
+console.log("BMI: " + bmi.toFixed(2)); // 小数点以下2桁まで表示
